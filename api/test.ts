36750cced0aa004a34eacca81386b1a42969db49
HyeonJun Jeon <teddy3582@naver.com>
2025-12-17 17:18:33 +0900

    fix: rewrite전면 제거

--- api/test.ts
+++
@@ -1,6 +0,0 @@
-import type { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default function handler(req: VercelRequest, res: VercelResponse) {
-  console.log('[Test] Function called!');
-  res.status(200).json({ message: 'Test function works!', method: req.method, url: req.url });
-}
